1aeec87079d1bd2621505f6081ab56a27ee8682e
Anil Kumar <anilkumarsikwal25@gmail.com>
2026-05-14 17:38:03 +0530

    add final build after changes in web.php file

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,34 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
-    $indexPath = public_path('index.html');
+    $indexPath = public_path('build/index.html');
     if (File::exists($indexPath)) {
-        return response(file_get_contents($indexPath))
+        $html = file_get_contents($indexPath);
+        // Fix asset paths - change /assets/ to /build/assets/
+        $html = str_replace('/assets/', '/build/assets/', $html);
+        $html = str_replace('/favicon.svg', '/build/favicon.svg', $html);
+        return response($html)
             ->header('Content-Type', 'text/html');
     }
     return view('welcome');
 });
 
-Route::get('/{path}', function (string $path) {
-    $filePath = public_path($path);
-
+Route::get('/build/{path}', function (string $path) {
+    $filePath = public_path('build/' . $path);
     if (File::exists($filePath) && is_file($filePath)) {
         return response()->file($filePath);
     }
+    abort(404);
+})->where('path', '.*');
 
-    $indexPath = public_path('index.html');
+Route::get('/{path}', function (string $path) {
+    // For SPA routes, serve index.html with fixed paths
+    $indexPath = public_path('build/index.html');
     if (File::exists($indexPath)) {
-        return response(file_get_contents($indexPath))
+        $html = file_get_contents($indexPath);
+        $html = str_replace('/assets/', '/build/assets/', $html);
+        $html = str_replace('/favicon.svg', '/build/favicon.svg', $html);
+        return response($html)
             ->header('Content-Type', 'text/html');
     }
 
